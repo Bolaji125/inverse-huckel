c20576dfbcdb42e01e02d1f46b4b8f9f7253ee5c
Bolaji125 <ogunnaikebolaji@gmail.com>
2024-04-22 14:00:13 +0100

    Calculate the MO energies of allyl radical and cyclopropenyl radical

--- inverse_huckel/main.py
+++ inverse_huckel/main.py
@@ -52,3 +52,41 @@ print("\nEigenvectors (each column represents a wavefunction):")
 print(wavefunctions)
 
 #workshop
+#question 1
+def allyl_radical_energy(alpha, beta):
+    E1 = alpha + np.sqrt(2) * beta
+    E2 = alpha
+    E3 = alpha - np.sqrt(2) * beta
+    return E1, E2, E3
+
+def cyclopropenyl_radical_energy(alpha, beta):
+    E1 = alpha + 2 * beta # can this also be called E1?
+    E2 = alpha - beta
+    E3 = alpha - beta
+    return E1, E2, E3
+
+# Constants for allyl radical
+alpha_allyl = -1.0
+beta_allyl = -0.5
+
+# Constants for cyclopropenyl radical
+alpha_cyclopropenyl = -1.0
+beta_cyclopropenyl = -0.5
+
+# Calculate molecular orbital energies for allyl radical
+E_allyl = allyl_radical_energy(alpha_allyl, beta_allyl)
+print("Molecular orbital energies for allyl radical:")
+print("E_allyl1:", E_allyl[0])
+print("E_allyl2:", E_allyl[1])
+print("E_allyl3:", E_allyl[2])
+
+# Calculate molecular orbital energies for cyclopropenyl radical
+E_cyclopropenyl = cyclopropenyl_radical_energy(alpha_cyclopropenyl, beta_cyclopropenyl)
+print("\nMolecular orbital energies for cyclopropenyl radical:")
+print("E_cyclopropenyl1:", E_cyclopropenyl[0])
+print("E_cyclopropenyl2:", E_cyclopropenyl[1])
+print("E_cyclopropenyl3:", E_cyclopropenyl[2])
+
+#question 4
+
+
