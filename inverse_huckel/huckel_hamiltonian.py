736d87556879d73bf98a0310fc86fc9d6984ba62
Bolaji125 <ogunnaikebolaji@gmail.com>
2024-04-22 17:27:06 +0100

    created a Hamiltonian class and cleaned up the main.py code

---
+++ inverse_huckel/huckel_hamiltonian.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+class HuckelHamiltonian:
+    def __init__(self, n, alpha, beta):
+        self.H = self.setup_hamiltonian(n, alpha, beta)
+    
+    def setup_hamiltonian(self, n, alpha, beta):
+        """
+        Constructs the Huckel Hamiltonian matrix for a cyclic pi system.
+
+        Args:
+        - n: Number of atoms in the cyclic pi system.
+        - alpha: Alpha parameter (energy of each isolated atomic orbital).
+        - beta: Beta parameter (resonance integral between adjacent atoms).
+
+        Returns:
+        - H: Huckel Hamiltonian matrix.
+        """
+        H = np.zeros((n, n))
+        for i in range(n):
+            H[i, i] = alpha
+            H[(i+1) % n, i] = beta
+            H[i, (i+1) % n] = beta
+        return H
+
+    def solve_huckel_hamiltonian(self):
+        """
+        Solves the Huckel Hamiltonian for a cyclic pi system.
+
+        Args:
+        - n: Number of atoms in the cyclic pi system.
+        - alpha: Alpha parameter (energy of each isolated atomic orbital).
+        - beta: Beta parameter (resonance integral between adjacent atoms).
+
+        Returns:
+        - energies: Eigenvalues of the Huckel Hamiltonian.
+        - wavefunctions: Eigenvectors of the Huckel Hamiltonian.
+        """
+        #H = huckel_hamiltonian(n, alpha, beta)
+        energies, wavefunctions = np.linalg.eigh(self.H)
+        return energies, wavefunctions
--- inverse_huckel/main.py
+++ inverse_huckel/main.py
@@ -2,49 +2,57 @@
 #create a script that evaluates the huckel hamiltonian(solves it) 
 
 import numpy as np
+import huckel_hamiltonian
 
-def huckel_hamiltonian(n, alpha, beta):
-    """
-    Constructs the Huckel Hamiltonian matrix for a cyclic pi system.
+# def huckel_hamiltonian(n, alpha, beta):
+#     """
+#     Constructs the Huckel Hamiltonian matrix for a cyclic pi system.
 
-    Args:
-    - n: Number of atoms in the cyclic pi system.
-    - alpha: Alpha parameter (energy of each isolated atomic orbital).
-    - beta: Beta parameter (resonance integral between adjacent atoms).
+#     Args:
+#     - n: Number of atoms in the cyclic pi system.
+#     - alpha: Alpha parameter (energy of each isolated atomic orbital).
+#     - beta: Beta parameter (resonance integral between adjacent atoms).
 
-    Returns:
-    - H: Huckel Hamiltonian matrix.
-    """
-    H = np.zeros((n, n))
-    for i in range(n):
-        H[i, i] = alpha
-        H[(i+1) % n, i] = beta
-        H[i, (i+1) % n] = beta
-    return H
+#     Returns:
+#     - H: Huckel Hamiltonian matrix.
+#     """
+#     H = np.zeros((n, n))
+#     for i in range(n):
+#         H[i, i] = alpha
+#         H[(i+1) % n, i] = beta
+#         H[i, (i+1) % n] = beta
+#     return H
 
-def solve_huckel_hamiltonian(n, alpha, beta):
-    """
-    Solves the Huckel Hamiltonian for a cyclic pi system.
+# def solve_huckel_hamiltonian(n, alpha, beta):
+#     """
+#     Solves the Huckel Hamiltonian for a cyclic pi system.
 
-    Args:
-    - n: Number of atoms in the cyclic pi system.
-    - alpha: Alpha parameter (energy of each isolated atomic orbital).
-    - beta: Beta parameter (resonance integral between adjacent atoms).
+#     Args:
+#     - n: Number of atoms in the cyclic pi system.
+#     - alpha: Alpha parameter (energy of each isolated atomic orbital).
+#     - beta: Beta parameter (resonance integral between adjacent atoms).
 
-    Returns:
-    - energies: Eigenvalues of the Huckel Hamiltonian.
-    - wavefunctions: Eigenvectors of the Huckel Hamiltonian.
-    """
-    H = huckel_hamiltonian(n, alpha, beta)
-    energies, wavefunctions = np.linalg.eigh(H)
-    return energies, wavefunctions
+#     Returns:
+#     - energies: Eigenvalues of the Huckel Hamiltonian.
+#     - wavefunctions: Eigenvectors of the Huckel Hamiltonian.
+#     """
+#     H = huckel_hamiltonian(n, alpha, beta)
+#     energies, wavefunctions = np.linalg.eigh(H)
+#    return energies, wavefunctions
+
+
+
 
 # Example usage:
-n = 6  # Number of atoms
+n = 3  # Number of atoms
 alpha = -1.0  # Energy of isolated atomic orbital
 beta = -0.5  # Resonance integral between adjacent atoms
 
-energies, wavefunctions = solve_huckel_hamiltonian(n, alpha, beta)
+H = huckel_hamiltonian.HuckelHamiltonian(n, alpha, beta)
+
+print(H.H)
+
+energies, wavefunctions = H.solve_huckel_hamiltonian()
 
 print("Eigenvalues:")
 print(energies)
